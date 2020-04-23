2a21f282f8124592830957db4d01a8c6f4d950df
Mohammed Karim <mkarom@jpl.nasa.gov>
2020-04-23 19:40:19 +0000

    update

--- docker/setup.sh
+++ docker/setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # clone spyddder-man to be moved to its final location by docker builder
-git clone -b master https://github.com/hysds/multi_acquisition_localizer.git
+git clone -b develop https://github.com/hysds/multi_acquisition_localizer.git
 
 # clone ariamh to be moved to its final location by docker builder
-git clone -b python3 https://github.com/hysds/ariamh.git
+git clone -b develop https://github.com/hysds/ariamh.git
 
